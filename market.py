1e922656ba63e01443852c181eae2794a95f3022
Katekani Madingana <madinganakn@gmail.com>
2024-02-26 00:03:24 -0800

    testing sending dictionary to a template

--- market.py
+++ market.py
@@ -11,4 +11,9 @@ def hello_world():
 
 @app.route('/market')
 def market_page():
-    return render_template('market.html', item_name='Phone')
+    items = [
+            {'id':1, 'name':'Phone', 'barcode':'98345983746':'price': 500},
+            {'id':2, 'name':'Laptop', 'barcode':'98734958943': 'price': 900},
+            {'id':3, 'name':'Keyboard', 'barcode':'98734589743', 'price': 150}
+            ]
+    return render_template('market.html', items=items)
