c352484cfc9a9ec2de6cbc420944cf6d980a9520
Katekani Madingana <madinganakn@gmail.com>
2024-02-27 03:57:17 -0800

    added database using sqllite3 and sqlalchemy, testing if webpages update with data from database

--- market.py
+++ market.py
@@ -2,7 +2,22 @@
 """ Market python file"""
 
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
+
 
 @app.route('/')
 @app.route('/home')
@@ -11,9 +26,6 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-            {'id':1, 'name':'Phone', 'barcode':'98345983746','price': 500},
-            {'id':2, 'name':'Laptop', 'barcode':'98734958943', 'price': 900},
-            {'id':3, 'name':'Keyboard', 'barcode':'98734589743', 'price': 150}
-            ]
+    with app.app_context():
+        Item.query_all()
     return render_template('market.html', items=items)
