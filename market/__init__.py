1576ebf607acae068f9e26fea680c4942375b37c
Katekani Madingana <madinganakn@gmail.com>
2024-02-29 09:46:31 +0200

    added form and form validations using wtforms and wtf.validators to validate the data

--- market/__init__.py
+++ market/__init__.py
@@ -5,5 +5,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'b9ca8ba5ee82fa0a1678f069'
 db = SQLAlchemy(app)
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
-from market import app
-from flask import render_template
-from market.models import Item
-
+from market import app, db
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from market.models import Item, User
+from market.forms import RegisterForm
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -12,4 +12,24 @@ def market_page():
     with app.app_context():
         items = Item.query.all()
     return render_template('market.html', items=items)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def registration_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        with app.app_context():
+            user_to_create = User(username=form.username.data,
+                                  email=form.email_address.data,
+                                  password_hash=form.password1.data)
+            db.session.add(user_to_create)
+            db.session.commit()
+            return redirect(url_for('market_page'))
+    if form.errors != {}:
+        for err in form.errors.values():
+            flash(f'The was an error {err}')
+    else:
+        print('no errors')
+    return render_template('register.html', form=form)
+
 
