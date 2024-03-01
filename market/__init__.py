de2fd9ee21ec070f648299aa0b21d529a05e328c
Katekani Madingana <madinganakn@gmail.com>
2024-03-01 13:55:23 +0200

    added login authentication using flask_login

--- market/__init__.py
+++ market/__init__.py
@@ -2,6 +2,7 @@
 """ Market python file"""
 
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 app = Flask(__name__)
@@ -9,4 +10,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'b9ca8ba5ee82fa0a1678f069'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -2,6 +2,8 @@ from market import app, db
 from flask import render_template, redirect, url_for, flash, get_flashed_messages
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
+from flask_login import login_user, logout_user
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -35,4 +37,23 @@ def registration_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
+    if form.validate_on_submit():
+        with app.app_context():
+            attempted_user = User.query.filter_by(username=form.username.data).first()
+            print(form.username.data)
+            print(attempted_user)
+            if attempted_user and attempted_user.check_password(
+                    attempted_password=form.password.data):
+                login_user(attempted_user)
+                flash(f'You are succesfully logged in: {attempted_user.username}', category='success')
+                return redirect(url_for('market_page'))
+            else:
+                flash('Username or password incorrect please try again', category='danger')
+
     return render_template('login.html', form=form) 
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash('you have been logged out', category='info')
+    return redirect(url_for('home_page'))
