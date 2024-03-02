6df1626944e1e8259636355e8365294904c1c938
Katekani Madingana <madinganakn@gmail.com>
2024-03-02 02:37:09 +0200

    fixed formatting using chatGPT

--- market/forms.py
+++ market/forms.py
@@ -3,18 +3,21 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
+
 class RegisterForm(FlaskForm):
+    """Form for user registration."""
 
     def validate_username(self, username_check):
+        """Validate if username already exists."""
         user = User.query.filter_by(username=username_check.data).first()
         if user:
-            raise ValidationError('Username already exists! please try a different username')
+            raise ValidationError('Username already exists! Please try a different username')
 
     def validate_email_address(self, email_address_check):
+        """Validate if email address already exists."""
         email = User.query.filter_by(email=email_address_check.data).first()
         if email:
-            raise ValidationError('Email address alredy exists')
-
+            raise ValidationError('Email address already exists')
 
     username = StringField(label='Username:', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email:', validators=[Email(), DataRequired()])
@@ -24,12 +27,21 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label='User Name:', validators=[DataRequired()])
+    """Form for user login."""
+
+    username = StringField(label='Username:', validators=[DataRequired()])
     password = PasswordField(label='Password:', validators=[DataRequired()])
     submit = SubmitField(label='Sign In')
 
+
 class PurchaseItemForm(FlaskForm):
+    """Form for purchasing an item."""
+    
     submit = SubmitField(label='Purchase Item!')
 
+
 class SellingForm(FlaskForm):
+    """Form for selling an item."""
+
     submit = SubmitField(label="Sell Item!")
+
--- market/models.py
+++ market/models.py
@@ -6,6 +6,8 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    """User model class."""
+
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email = db.Column(db.String(length=50), nullable=False, unique=True)
@@ -13,15 +15,17 @@ class User(db.Model, UserMixin):
     budget = db.Column(db.Integer(), nullable=False, default=50000)
     items = db.relationship('Item', backref='owned_user', lazy='dynamic')
 
-
     def can_sell(self, item_obj):
+        """Check if the user can sell the given item."""
         return item_obj in self.items
 
     def can_purchase(self, item_obj):
+        """Check if the user can purchase the given item."""
         return self.budget >= item_obj.price
 
     @property
     def pretty_budget(self):
+        """Format budget in a pretty way."""
         if len(str(self.budget)) >= 4:
             return 'R {:,.2f}'.format(self.budget)
         else:
@@ -29,16 +33,21 @@ class User(db.Model, UserMixin):
 
     @property
     def password(self):
+        """Get the user's password."""
         return self.password
 
     @password.setter
     def password(self, text_password):
+        """Set the user's password."""
         self.password_hash = bcrypt.generate_password_hash(text_password).decode('utf-8')
 
     def check_password(self, attempted_password):
+        """Check if the provided password is correct."""
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 class Item(db.Model):
+    """Item model class."""
+    
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
@@ -47,16 +56,18 @@ class Item(db.Model):
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def buy(self, user):
+        """Buy the item."""
         self.owner = user.id
         user.budget -= self.price
         db.session.commit()
 
     def sell(self, user):
+        """Sell the item."""
         self.owner = None
         user.budget += self.price
         db.session.commit()
 
-
     def __repr__(self):
+        """Representation of the item."""
         return f'Item {self.name}'
 
